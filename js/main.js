066e14fcbc437e02cb5452f7af4980487482a558
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2022-02-08 09:41:29 -0600

    Add setupOctaves and init functions

--- js/main.js
+++ js/main.js
@@ -3,13 +3,24 @@
 const startNotes = ['C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B'];
 
 const startNoteSelector = document.querySelector('#start-note');
+const octaveSelector = document.querySelector('#octave');
 
 const app = {
+  init() {
+    this.setupStartNotes();
+    this.setupOctaves();
+  },
   setupStartNotes() {
     startNotes.forEach(noteName =>{
       let noteNameOption = this.createElement('option', noteName);
       startNoteSelector.appendChild(noteNameOption);
     });
+  },
+  setupOctaves() {
+    for (let i = 1; i <= 7; i++) {
+      let octaveNumber = this.createElement('option', i);
+      octaveSelector.appendChild(octaveNumber);
+    }
   },
   createElement(elementName, content) {
     let element = document.createElement(elementName);
@@ -18,4 +29,5 @@ const app = {
   }
 };
 
-app.setupStartNotes();
+app.init();
+
